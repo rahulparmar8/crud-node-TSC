07c2f035fbbd05e37c06f072945e3f5c5d7e8cbb
rahulparmar8 <you@example.com>
2022-04-11 19:08:23 +0530

    Add Validation

--- build/controllers/users.js
+++ build/controllers/users.js
@@ -13,23 +13,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const users_1 = __importDefault(require("../models/users"));
+const express_validation_1 = require("express-validation");
 class Product {
     constructor() {
         // Add Product Data //
-        this.AddProduct = (req, res) => __awaiter(this, void 0, void 0, function* () {
+        this.AddProduct = (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            var _a;
+            const schema = express_validation_1.Joi.object()
+                .keys({
+                product_name: express_validation_1.Joi.string()
+                    .min(3)
+                    .max(40)
+                    .required(),
+                category_id: express_validation_1.Joi.string()
+                    .min(3)
+                    .max(40)
+                    .required(),
+                product_price: express_validation_1.Joi.number()
+                    .integer()
+                    .required(),
+                image_url: express_validation_1.Joi.string()
+                    .required(),
+            });
             try {
-                const { product_name, category_id, product_price, image_url } = req.body;
-                const data = new users_1.default({
-                    product_name: product_name,
-                    category_id: category_id,
-                    product_price: product_price,
-                    image_url: image_url,
-                });
-                const result = yield data.save();
-                res.status(200).json({ message: "Data Saved" });
+                const checkValidation = schema.validate(req.body);
+                // console.log(checkValidation);
+                if (checkValidation.error) {
+                    res.status(422)
+                        .send((_a = checkValidation.error) === null || _a === void 0 ? void 0 : _a.details[0].message);
+                }
+                else {
+                    console.log("hello");
+                    const { product_name, category_id, product_price, image_url } = req.body;
+                    const data = new users_1.default({
+                        product_name: product_name,
+                        category_id: category_id,
+                        product_price: product_price,
+                        image_url: image_url,
+                    });
+                    const result = yield data.save();
+                    res.status(200).json({ message: "Data Saved" });
+                }
             }
             catch (error) {
-                console.log(error);
+                // console.log(req.body);
             }
         });
         // Get All Product Data //
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,22 +1,47 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import ProductModel from "../models/users";
+import { Joi } from 'express-validation';
 
 export default class Product {
 
     // Add Product Data //
-    AddProduct = async (req: Request, res: Response) => {
+    AddProduct = async (req: Request, res: Response, next: NextFunction) => {
+        const schema = Joi.object()
+            .keys({
+                product_name: Joi.string()
+                    .min(3)
+                    .max(40)
+                    .required(),
+                category_id: Joi.string()
+                    .min(3)
+                    .max(40)
+                    .required(),
+                product_price: Joi.number()
+                    .integer()
+                    .required(),
+                image_url: Joi.string()
+                    .required(),
+            })
         try {
-            const { product_name, category_id, product_price, image_url } = req.body
-            const data = new ProductModel({
-                product_name: product_name,
-                category_id: category_id,
-                product_price: product_price,
-                image_url: image_url,
-            });
-            const result = await data.save();
-            res.status(200).json({ message: "Data Saved" })
+            const checkValidation = schema.validate(req.body);
+            if (checkValidation.error) {
+                res.status(422)
+                    .send(checkValidation.error?.details[0].message);
+            } else {
+                const { product_name, category_id, product_price, image_url } = req.body
+                const data = new ProductModel({
+                    product_name: product_name,
+                    category_id: category_id,
+                    product_price: product_price,
+                    image_url: image_url,
+                });
+                const result = await data.save();
+                res.status(200).json({ message: "Data Saved" })
+            }
+
         } catch (error) {
-            console.log(error);
+
+            // console.log(req.body);
         }
     }
     // Get All Product Data //
